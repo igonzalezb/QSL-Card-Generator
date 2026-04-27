02f16fea8e42e0f784b2fb0e01f755808d82016a
igonzalezb <igonzalezb@users.noreply.github.com>
2026-04-27 20:47:02 -0300

    new: improve logging by limiting log file size

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import sys
 import logging
+from logging.handlers import RotatingFileHandler
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 from core.utils import resource_path
 from core.utils import LOG_FILE
+from ui.main_window import QSLGeneratorApp
 
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+file_handler = RotatingFileHandler(
+    LOG_FILE, 
+    maxBytes=1 * 1024 * 1024,
+    backupCount=1,           
+    encoding='utf-8'
 )
 
-from ui.main_window import QSLGeneratorApp
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[file_handler]
+)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
